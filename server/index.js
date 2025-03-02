82e6d53d05052bde66d58ed5142faddb1f618b15
amaldeeppj <amaldeeppjcoc1@gmail.com>
2025-03-02 08:26:48 +0530

    2

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 const http = require('http')
+const express = require('exoress')
+const { Server: SocketServer } = require('socket.io')
+
+const app = express()
+const server = http.createServer(app);
+const io = new SocketServer({
+    cors: '*'
+})
+
+io.attach(server); 
+
+server.listen(9000, () => console.log('docker running in port 9000'))
